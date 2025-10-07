ff6c2d97c2ab5ef1de4e253b3cb7500b0eb774f9
Geraldo <geraldo7888@gmail.com>
2025-10-07 00:17:19 -0500

    Se agrega app.js correspondiente a la actividad 1: Acceso a un sistema - trasnferencia

---
+++ app.js
@@ -0,0 +1,20 @@
+// 1. Acceso a un sistema
+
+let ingresoUsuario = prompt("Ingrese Usuario:");
+let ingresoPass = prompt("Ingrese Contraseña:"); 
+
+let usuario = "admin";
+let pass = "1234";
+
+if ((usuario == ingresoUsuario) && (pass == ingresoPass)) {
+    alert("Acceso Correcto");
+} else {
+    alert("Acceso Denegado");
+}
+
+// Pregunta de análisis: ¿Qué operador lógico usarías para verificar que se cumplan
+// ambas condiciones?
+
+// R: El operador que a mi parecer es el mas correcto es && 
+// porque los datos de ingreso del usuario deben coincidir, al igual que la contraseña, 
+// lo que quiere decir que ambos valores deben ser ciertos.
